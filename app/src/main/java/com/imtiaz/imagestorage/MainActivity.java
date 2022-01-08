0db5107675241524bd71411c89454258ae81fe17
Imtiaz16041997 <imtiazeemel42@gmail.com>
2022-01-08 16:47:59 +0600

    saving the captured image

--- app/src/main/java/com/imtiaz/imagestorage/MainActivity.java
+++ app/src/main/java/com/imtiaz/imagestorage/MainActivity.java
@@ -1,6 +1,7 @@
 package com.imtiaz.imagestorage;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -8,6 +9,7 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -56,6 +58,19 @@ public class MainActivity extends AppCompatActivity {
     private void openCamera() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent, CAMERA_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode ==CAMERA_REQUEST_CODE){
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            SelectimageView.setImageBitmap(bitmap);
+        }
+
+
+
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
