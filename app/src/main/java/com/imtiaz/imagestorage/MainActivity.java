320c7a65e808b803432a187fca9311d026fea4b0
Imtiaz16041997 <imtiazeemel42@gmail.com>
2022-01-08 16:36:46 +0600

    first commit

---
+++ app/src/main/java/com/imtiaz/imagestorage/MainActivity.java
@@ -0,0 +1,76 @@
+package com.imtiaz.imagestorage;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public static final int CAMERA_REQUEST_CODE = 102;
+    ImageView SelectimageView;
+    Button camera,gallery;
+    private static int CAMERA_PERM_CODE = 101;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SelectimageView = findViewById(R.id.imageView);
+        camera = findViewById(R.id.button1);
+        gallery = findViewById(R.id.button2);
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                askCameraPermission();
+                //Toast.makeText(MainActivity.this, "Camera Open Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Gallery Open Request", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void askCameraPermission() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
+        }else{
+            openCamera();
+        }
+    }
+
+    private void openCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if(requestCode == CAMERA_PERM_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                //OpenCamera
+            }else {
+                Toast.makeText(this, "Camera permission is required to use camera", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
+}
